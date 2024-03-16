b8cb6c8c00e69a7650d3db68648f12378b11585a
amulet1989 <muletdelosreyes89@gmail.com>
2024-03-16 10:51:27 -0300

    fine tunning

--- train_swin_with_upenn.py
+++ train_swin_with_upenn.py
@@ -338,7 +338,7 @@ model_inferer = partial(
     overlap=infer_overlap,
 )
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
 
 
